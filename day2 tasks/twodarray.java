e8429528601412cdc363eab98c057eebd3311caa
Surya-BCA <2322ja56@kgcas.com>
2025-06-06 14:43:59 +0530

    Add files via upload

---
+++ day2 tasks/twodarray.java
@@ -0,0 +1,12 @@
+class Array {
+    public static void main(String[] args) {
+        int[][] numbers = {{1,2},{10,20}}; 
+        for(int i=0;i<2;i++) {
+        	for(int j=0;j<2;j++) {
+        		System.out.print(numbers[i][j]+"  ");
+        	
+        	}
+        	System.out.println(" ");
+        }
+    }
+}
